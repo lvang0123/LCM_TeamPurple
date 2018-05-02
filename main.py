bdd9dbbcd39304ff86afc9ecca3d7c29580b9cc6
CIS126Spring2018DanMoen <37819489+CIS126Spring2018DanMoen@users.noreply.github.com>
2018-05-01 21:03:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,16 +3,23 @@ num2 = int(input("Enter number 2: "))
 
 print()
 
+#if the second number entered is larger than the first
 if(num1 < num2):
+  #the bigger of the two is logically the second one, and therefore BIGGER is assigned this value
   bigger = num2
+  #the else statement of this if statement
 else:
   bigger = num1
       
+#while true, loop initialization
 while True:
   if((bigger % num2 == 0) and (bigger % num1 ==0)):
+#if BIGGER is both devisible by both input numbers without a remainder, assign LCM as the value of BIGGER
     lcm = bigger
+#break out of loop
     break
-  
+  #else add 1 to bigger, and continue the loop
   bigger += 1
   
+#when broken out of, print the value of LCM
 print("Least Common Multiple: ", lcm)
